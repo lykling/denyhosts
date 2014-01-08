a9ff59b0464f4dacea76b2d7cf5060665ececd2c
lykling <lykling.lyk@gmail.com>
2014-01-08 13:50:09 +0000

    add tls support for gmail smtp server

--- DenyHosts/util.py
+++ DenyHosts/util.py
@@ -9,31 +9,34 @@ from types import IntType
 
 debug = logging.getLogger("util").debug
 
+
 def setup_logging(prefs, enable_debug, verbose, daemon):
     if daemon:
         daemon_log = prefs.get('DAEMON_LOG')
         if daemon_log:
-            # define a Handler which writes INFO messages or higher to the sys.stderr
+            # define a Handler
+            # which writes INFO messages or higher to the sys.stderr
             fh = logging.FileHandler(daemon_log, 'a')
             fh.setLevel(logging.DEBUG)
-            formatter = logging.Formatter(prefs.get('DAEMON_LOG_MESSAGE_FORMAT'),
-                                          prefs.get('DAEMON_LOG_TIME_FORMAT'))
+            msg_fmt = prefs.get('DAEMON_LOG_MESSAGE_FORMAT')
+            time_fmt = prefs.get('DAEMON_LOG_TIME_FORMAT')
+            formatter = logging.Formatter(msg_fmt, time_fmt)
             fh.setFormatter(formatter)
             # add the handler to the root logger
             logging.getLogger().addHandler(fh)
             if enable_debug:
                 # if --debug was enabled provide gory activity details
                 logging.getLogger().setLevel(logging.DEBUG)
-                #prefs.dump_to_logger()                              
+                #prefs.dump_to_logger()
             else:
                 # in daemon mode we always log some activity
                 logging.getLogger().setLevel(logging.INFO)
-                
+
             info = logging.getLogger("denyhosts").info
             info("DenyHosts launched with the following args:")
             info("   %s", ' '.join(sys.argv))
             prefs.dump_to_logger()
-    else: # non-daemon
+    else:  # non-daemon
         try:
             # python 2.4
             logging.basicConfig(format="%(message)s")
@@ -45,7 +48,7 @@ def setup_logging(prefs, enable_debug, verbose, daemon):
 
         debug = logging.getLogger("denyhosts").debug
         info = logging.getLogger("denyhosts").info
-            
+
         if verbose:
             logging.getLogger().setLevel(logging.INFO)
         elif enable_debug:
@@ -56,35 +59,40 @@ def setup_logging(prefs, enable_debug, verbose, daemon):
 
 def die(msg, ex=None):
     print msg
-    if ex: print ex
+    if ex:
+        print ex
     sys.exit(1)
 
 
-def is_true(s):
-    s = s.lower()
-    if s in ('1', 't', 'true', 'y', 'yes'):
+def is_true(pattern):
+    pattern = pattern.lower()
+    if pattern in ('1', 't', 'true', 'y', 'yes'):
         return True
     else:
         return False
 
-def is_false(s):
-    return not is_true(s)
+
+def is_false(pattern):
+    return not is_true(pattern)
 
 
 def calculate_seconds(timestr, zero_ok=False):
     # return the number of seconds in a given timestr such as 1d (1 day),
     # 13w (13 weeks), 5s (5seconds), etc...
-    if type(timestr) is IntType: return timestr
-    
+    if type(timestr) is IntType:
+        return timestr
+
     m = TIME_SPEC_REGEX.search(timestr)
     if not m:
-        raise Exception, "Invalid time specification: string format error: %s", timestr
+        except_msg = "Invalid time specification: string format error: %s"
+        raise Exception, except_msg, timestr
 
     units = int(m.group('units'))
-    period = m.group('period') or 's' # seconds is the default
+    period = m.group('period') or 's'  # seconds is the default
 
     if units == 0 and not zero_ok:
-        raise Exception, "Invalid time specification: units = 0"
+        except_msg = "Invalid time specification: units = 0"
+        raise Exception, except_msg, units
 
     seconds = units * TIME_SPEC_LOOKUP[period]
     #info("converted %s to %ld seconds: ", timestr, seconds)
@@ -94,7 +102,7 @@ def calculate_seconds(timestr, zero_ok=False):
 def parse_host(line):
     # parses a line from /etc/hosts.deny
     # returns the ip address
-    
+
     # the deny file can be in the form:
     # 1) ip_address
     # 2) sshd: ip_address
@@ -108,8 +116,10 @@ def parse_host(line):
         vals = line.split(":")
 
         # we're only concerned about the ip_address
-        if len(vals) == 1: form = vals[0]
-        else: form = vals[1]
+        if len(vals) == 1:
+            form = vals[0]
+        else:
+            form = vals[1]
 
         host = form.strip()
     except:
@@ -117,7 +127,7 @@ def parse_host(line):
     return host
 
 
-def send_email(prefs, report_str):           
+def send_email(prefs, report_str):
     recipients = prefs['ADMIN_EMAIL'].split(',')
 
     msg = """From: %s
@@ -132,22 +142,25 @@ Date: %s
 
     msg += report_str
     try:
-        smtp = SMTP(prefs.get('SMTP_HOST'),
-                    prefs.get('SMTP_PORT'))
+        host = prefs.get('SMTP_HOST')
+        port = prefs.get('SMTP_PORT')
+        smtp = SMTP(host, port)
+        if host == 'smtp.gmail.com' or host == 'mail.google.com':
+            smtp.starttls()
 
         username = prefs.get('SMTP_USERNAME')
         password = prefs.get('SMTP_PASSWORD')
 
         if username and password:
             smtp.login(username, password)
-    
+
         smtp.sendmail(prefs.get('SMTP_FROM'),
                       recipients,
                       msg)
         debug("sent email to: %s" % prefs.get("ADMIN_EMAIL"))
-    except Exception, e:
+    except Exception, err:
         print "Error sending email"
-        print e
+        print err
         print "Email message follows:"
         print msg
 
